b583eb1f2478a45ef787e0dacee0beb06afb88e3
inusky <inuskydotcom@gmail.com>
2026-02-18 13:46:53 +0530

    Fix issues

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,8 +1,10 @@
-export default defineNuxtRouteMiddleware(async () => {
+export default defineNuxtRouteMiddleware(async (to) => {
   const user: any = await $fetch('/api/v1/auth/me');
 
-  if (!user.authenticated) {
+  if (!user.authenticated && to.path !== '/auth/sign-in') {
     return navigateTo('/auth/sign-in');
+  } else if (user.authenticated && to.path === '/auth/sign-in') {
+    return navigateTo('/');
   } else {
     return;
   }
